5b0a608ff35d11ca4e4e5ab3c2aa893ce6c3dd96
Brahms95 <shigsv@yandex.ru>
2021-09-27 18:51:21 +0300

    adding realization

--- statistict.h
+++ statistict.h
@@ -253,3 +253,175 @@ class PCT90 :public Update{
 
 };
 
+//=======Другая реализация==============
+/*
+class Base_min {
+	public:
+
+	virtual  const char*  _min()=0;
+	virtual const char * name_min() const =0;
+	virtual  const char*  _max()=0;
+	virtual const char * name_max() const =0;
+
+
+    std::vector<int>Vector_int; 
+    std::vector<double>Vector_double; 
+		
+};
+
+class Medium {
+	public:
+
+	virtual  const char*  _mean()=0;
+	virtual const char * name_mean() const =0;
+	virtual  const char*  _str()=0;
+	virtual const char * name_str() const =0;
+	virtual  const char*  _pct()=0;
+	virtual const char * name_pct() const =0;
+	
+
+    std::vector<int>Vector_int; 
+    std::vector<double>Vector_double; 
+		
+};
+class Statistict_base :public Base_min{
+
+	using Base::Vector_int;
+	using Base::Vector_double;
+
+
+	const char*  _min() override {
+
+		std::stringstream s;
+		if (!Vector_int.empty()){
+
+			 std::vector<int>::iterator it; // объявляем итератор
+			it = min_element(Vector_int.begin(), Vector_int.end());
+			int min_ =Vector_int[std::distance( Vector_int.begin(), min_element(Vector_int.begin(), Vector_int.end()))];
+			s << min_;
+			m_min = s.str().c_str();
+
+		}
+		else{
+			std::vector<double>::iterator it; // объявляем итератор
+    		it = min_element(Vector_double.begin(), Vector_double.end());
+			double min_ =Vector_double[std::distance( Vector_double.begin(), min_element(Vector_double.begin(), Vector_double.end()))];
+			s << min_;
+			m_min = s.str().c_str();
+		}
+
+		return  m_min;		
+	}
+
+	const char * name_min() const override {
+		return "min";
+	}
+
+	const char*  _max() override {
+		char return_char ;
+		std::stringstream s;
+	
+		if (!Vector_int.empty()){
+
+			std::vector<int>::iterator it; // объявляем итератор
+    		it = max_element(Vector_int.begin(), Vector_int.end());
+            int max_vector =Vector_int[std::distance( Vector_int.begin(), max_element(Vector_int.begin(), Vector_int.end()))];
+	        s << max_vector;
+			m_max = s.str().c_str();
+		
+        }
+		else{
+			std::vector<double>::iterator it; // объявляем итератор
+			it = max_element(Vector_double.begin(), Vector_double.end());
+			double max_vector =Vector_double[std::distance( Vector_double.begin(), max_element(Vector_double.begin(), Vector_double.end()))];
+ 			s << max_vector;
+			m_max = s.str().c_str();
+		
+		}
+		return  m_max;	
+
+	}
+	const char* name_max() const  override { return "max";}
+
+    private:
+	const char* m_min;
+	const char* m_max;
+
+};
+
+class Statistict_medium :public Medium{
+	public:
+		Statistict_medium(int pct) :_pct_value(pct){};
+		using Medium::Vector_int;
+		using Medium::Vector_double;
+		const char*  _str()  override{
+			double sum = 0;
+			std::stringstream s;
+			for ( double i : Vector_int ) sum += i;
+			sum= sum / Vector_int.size();		
+			double var = 0;
+			for ( double i : Vector_int ) {
+  				var += (i- sum) * (i - sum);
+			}
+			var /= Vector_int.size();
+
+ 			s << sqrt(var);
+			sqrt_ = s.str().c_str();
+		
+		return sqrt_;
+		}
+
+		const char* name_str() const  override {
+			return "std";
+		}
+	
+		const char*  _pct()  override{
+	
+    		std::stringstream s;
+    		const auto n  = Vector_int.size();
+			const auto id = (n - 1) * _pct_value;
+			const auto lo = floor(id);
+			const auto hi = ceil(id);
+			const auto qs = Vector_int[lo];
+			const auto h  = (id - lo);
+			s << (1.0 - h) * qs + h * Vector_int[hi];
+			pct_ = s.str().c_str();
+   		return pct_ ;
+		}
+
+		const char* name_pct() const  override {
+			return "pct90";
+		}
+	
+		const char*  _mean()  override{
+			std::stringstream s;
+			if (!Vector_int.empty()){
+				double sum = 0;
+
+			for ( int i : Vector_int ) sum += i;
+			s << sum / Vector_int.size();
+			str_ = s.str().c_str();
+    	}
+    	else{
+        	double sum = 0;
+			for ( int i : Vector_double ) sum += i;
+			s << sum / Vector_double.size();
+			str_ = s.str().c_str();
+    	}
+		return str_;		
+	}
+
+	const char* name_mean() const  override {
+			return "mean";
+		}	
+
+	private:
+	std::vector<double> values_std;
+    
+	int _pct_value;
+	const char* pct_;
+
+		const char* sqrt_;
+		const char* str_;	
+};
+*/
